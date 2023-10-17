469c80b8fb3e8f93b8730b1fdf2b6dd1ece7561c
karmiski <karolinamiski@gmail.com>
2023-10-17 16:02:46 +0200

    wcale nie zmieniam tego40 min przed zajęciami xd

--- zadania do prezentowania/zad9.py
+++ zadania do prezentowania/zad9.py
@@ -1,24 +1,6 @@
 #Napisać program wyszukujący liczby zaprzyjaźnione mniejsze od miliona. 
 N =  1000000
 num = list(range(0,N))
-'''
-#algorytm najprostszy - O(n*sqrt(n)), dla miliona zbyt wolny
-for i in range(2,N):
-    a = i
-    x = 2
-    num[i] = 1
-    while(x*x<a):
-        if(a%x) == 0:
-            num[i] += x + int(a/x)
-        x+=1
-    if(x*x == a): 
-        num[i] += x
-for i in range(2,N):
-    if(num[i] < N):
-        if(num[num[i]] < N):
-            if(i == num[num[i]] and i < num[i]):
-                print(i,num[i])
-'''
 for i in range(2,N):
     a = i
     x = 2
